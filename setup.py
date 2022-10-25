bf4784ae7f25dce2720fc0805d7283daf7a18b81
root <root@dsde586.fornex.org>
2022-10-25 09:14:16 +0000

    Sobolev:FixPythonVersion

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 import setuptools
 
 __author__ = 'Sobolev Andrey <email.asobolev@gmail.com>'
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -24,5 +24,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.9',
 )
